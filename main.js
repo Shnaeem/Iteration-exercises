05e43accac92b62696ed1a42b89c5039fc0e3b5d
TKH STUDENT 12 <tkhstudent12@TKHs-Air-3.home>
2020-09-30 21:11:11 -0400

    classwork complete

--- main.js
+++ main.js
@@ -2,10 +2,37 @@
 //Using map, write a function that takes an array of numbers
 //and makes them strings
 
+let numbers = [1, 2, 3, 4, 5];
+function convertToString(arr) {
+let fred = arr.map(String);
+return fred;
+}
+console.log(convertToString(numbers))
+
+
 //Exercise #2
 //You must use forEach for this exercise
 //Write a function that accepts an array and returns a new array
 //with all the values in the array passed to the function doubled
+
+let numbers = [1,2,3,4];
+function doublevalues(arr){
+    let newarray = [];
+    arr.forEach( function(value){ newarray.push(value*2);});
+  	return newarray;
+}
+
+console.log(doublevalues([1,2,3,4]));
+
+
+function acceptArrayDouble (num){
+    let google = num.forEach(element => console.log(element * 2))
+    return google;
+}
+
+acceptArrayDouble(numbers);
+
+
 
 //Exercise #3
 //Write a function called firstAndLast which accepts
@@ -14,6 +41,16 @@
 //Example: ['winning', 'tomato'] => ['wg', 'to']
 //hint: forEach show be used here
 
-//Bonus:
-//Write a for loop that outputs this:
-// 1 1 1 2 2 2 3 3 3 4 4 4
+let array1 = ['winning', 'tomato'];
+let result=[];
+array1.forEach(function(currentString) {
+    result.push(currentString.charAt(0) + currentString.charAt(currentString.length - 1)); //First + Last character
+});
+console.log(result);
+
+
+for (let i = 1 ; i < 5 ; i++){
+    for (let p = 0; p < 3 ; p++){
+        console.log(i);
+    }
+}
